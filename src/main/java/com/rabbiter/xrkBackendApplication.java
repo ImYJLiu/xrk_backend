02cc0c110c92a3c177f4031272a8d234bc455140
ImYJLiu <1627465617@qq.com>
2024-12-21 13:26:11 +0800

    git init

---
+++ src/main/java/com/rabbiter/xrkBackendApplication.java
@@ -0,0 +1,12 @@
+package com.rabbiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class xrkBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(xrkBackendApplication.class, args);
+	}
+
+}
